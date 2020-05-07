5bdf673053b84cee17cde37ec38c96c9dec3abd6
Calvin Cheng <wc15799@my.bristol.ac.uk>
2020-05-07 17:56:43 +0800

    Added chapter link functionality

--- script.js
+++ script.js
@@ -3,16 +3,19 @@ Thanks:
 https://stackoverflow.com/questions/49916259/show-element-when-in-viewport-on-scroll
 */
 
-var myElement = document.querySelector( '.article-info-wrapper' );
-let chapterElement = document.querySelector( '.chapter-list-wrapper' );
+let aboveElement = document.querySelector( '.article-cover' );
+let infoHeight = document.querySelector( '.article-info-wrapper' ).offsetHeight;
+let chapterWrapper = document.querySelector( '.chapter-list-wrapper' );
 
 // Listen for the scroll event
 document.addEventListener( 'scroll', event => {
-  // Check the viewport status
-  if( inViewport( myElement ) ){
-    chapterElement.style.opacity = 1;
+  // Show/Hide chapter list
+  if( inViewport( aboveElement ) && window.innerWidth >= 1024 ) {
+    chapterWrapper.style.opacity = 1;
+    chapterWrapper.style.visibility = 'visible';
   } else {
-    chapterElement.style.opacity = 0;
+    chapterWrapper.style.opacity = 0;
+    chapterWrapper.style.visibility = 'hidden';
   }
 })
 
@@ -21,6 +24,26 @@ function inViewport( element ){
   let height = element.offsetHeight;
   let bb = element.getBoundingClientRect();
   // Return true when element div is scrolled above viewport
-  return (bb.top < -height);
+  return (bb.top < -(height + infoHeight));
 }
 
+function chapterScroll( chapter ) {
+  let n = getChildIndex( chapter ); // Get n-th child
+  let heading = document.querySelector( `h3:nth-of-type(${n / 2})` );
+  let y = heading.offsetTop;
+  let offset = 170;
+  window.scrollTo(0, y - offset)
+  return null;
+}
+
+function getChildIndex( child ) {
+  // Returns 'n' for the n-th child
+  let n = 1;
+  while ( (child = child.previousSibling) != null ) {
+    n++;
+  }
+  return n;
+}
+
+// document.querySelector('.chapter-list-wrapper li:nth-of-type(2)')
+//
