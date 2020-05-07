cb76da7388cefefb0917dab020e90ca2fc24c4ae
Calvin Cheng <wc15799@my.bristol.ac.uk>
2020-05-07 23:57:55 +0800

    Chapter highlighting

--- script.js
+++ script.js
@@ -1,49 +1,57 @@
+'use strict'
+
 /* 
 Thanks: 
 https://stackoverflow.com/questions/49916259/show-element-when-in-viewport-on-scroll
 */
 
-let aboveElement = document.querySelector( '.article-cover' );
-let infoHeight = document.querySelector( '.article-info-wrapper' ).offsetHeight;
-let chapterWrapper = document.querySelector( '.chapter-list-wrapper' );
-
-// Listen for the scroll event
-document.addEventListener( 'scroll', event => {
-  // Show/Hide chapter list
-  if( inViewport( aboveElement ) && window.innerWidth >= 1024 ) {
-    chapterWrapper.style.opacity = 1;
-    chapterWrapper.style.visibility = 'visible';
-  } else {
-    chapterWrapper.style.opacity = 0;
-    chapterWrapper.style.visibility = 'hidden';
-  }
-})
-
 function inViewport( element ){
   // Get the elements position relative to the viewport
+  let infoHeight = document.querySelector( '.article-info-wrapper' ).offsetHeight;
   let height = element.offsetHeight;
   let bb = element.getBoundingClientRect();
   // Return true when element div is scrolled above viewport
   return (bb.top < -(height + infoHeight));
 }
 
-function chapterScroll( chapter ) {
-  let n = getChildIndex( chapter ); // Get n-th child
-  let heading = document.querySelector( `h3:nth-of-type(${n / 2})` );
-  let y = heading.offsetTop;
-  let offset = 170;
-  window.scrollTo(0, y - offset)
-  return null;
+function scrollToId( id ) {
+  let heading = document.getElementById( id );
+  heading.scrollIntoView({behavior: 'smooth'});
 }
 
-function getChildIndex( child ) {
-  // Returns 'n' for the n-th child
-  let n = 1;
-  while ( (child = child.previousSibling) != null ) {
-    n++;
+function makeHeadingHeights() {
+  let heights = [0];
+  let i = 1;
+  let offset = 168;
+  while (document.querySelector( `.chapter:nth-of-type(${i})` ) != null ) {
+    let heading = document.querySelector( `h3:nth-of-type(${i})` );
+    heights[i] = heading.getBoundingClientRect().top + window.scrollY - offset;
+    i++;
   }
-  return n;
+  return heights;
 }
 
-// document.querySelector('.chapter-list-wrapper li:nth-of-type(2)')
-//
+// Listen for the scroll event
+document.addEventListener( 'scroll', event => {
+  // Show/Hide chapter list
+  let aboveElement = document.querySelector( '.article-cover' );
+  let chapterWrapper = document.querySelector( '.chapter-list-wrapper' );
+  if( inViewport( aboveElement ) && window.innerWidth >= 1024 ) {
+    chapterWrapper.style.opacity = 1;
+  } else {
+    chapterWrapper.style.opacity = 0;
+  }
+
+  // Highlight current chapter
+  let numChapters = document.querySelector( '.chapter-list-wrapper ul' ).childElementCount;
+  let n = numChapters;
+  while (window.scrollY < headingHeights[n] && n > 0) {
+    n--;
+  }
+  for (let i = 1; i <= numChapters; i++) {
+    document.querySelector( `.chapter:nth-of-type(${i}) span` ).classList.remove('active');
+  }
+  document.querySelector( `.chapter:nth-of-type(${n}) span` ).classList.add('active');
+})
+
+let headingHeights = makeHeadingHeights();
