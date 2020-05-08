687e3b6354ec65577007267cd01377ff7e47e860
Calvin Cheng <wc15799@my.bristol.ac.uk>
2020-05-08 14:55:03 +0800

    Added polyfill for scrollToView()

--- script.js
+++ script.js
@@ -5,24 +5,48 @@ Thanks:
 https://stackoverflow.com/questions/49916259/show-element-when-in-viewport-on-scroll
 */
 
-function inViewport( element ){
-  // Get the elements position relative to the viewport
-  let infoHeight = document.querySelector( '.article-info-wrapper' ).offsetHeight;
-  let height = element.offsetHeight;
-  let bb = element.getBoundingClientRect();
-  // Return true when element div is scrolled above viewport
-  return (bb.top < -(height + infoHeight));
+function scrollToId( id ) {
+  // Check if browser supports scroll function
+  let element = document.getElementById( id );
+  if ('scrollBehavior' in document.documentElement.style) {
+    element.scrollIntoView({behavior: 'smooth'});
+  } else {
+    let offset = parseInt(getComputedStyle(document.querySelector('html')).scrollPaddingTop);
+    let y = element.getBoundingClientRect().top + window.scrollY - offset;
+    smoothScrollTo(0, y, 600);
+  }
 }
 
-function scrollToId( id ) {
-  let heading = document.getElementById( id );
-  heading.scrollIntoView({behavior: 'smooth'});
-}
+window.smoothScrollTo = function(endX, endY, duration) {
+  // Polyfill for scrollIntoView in Safari/Edge
+  // https://stackoverflow.com/questions/49636727/why-is-scroll-behaviorsmooth-not-working-but-javascript-window-scroll-smooth-is
+  let startX = window.scrollX || window.pageXOffset,
+  startY = window.scrollY || window.pageYOffset,
+  distanceX = endX - startX,
+  distanceY = endY - startY,
+  startTime = new Date().getTime();
+
+  // Easing function
+  let easeInOutQuart = function(time, from, distance, duration) {
+      if ((time /= duration / 2) < 1) return distance / 2 * time * time * time * time + from;
+      return -distance / 2 * ((time -= 2) * time * time * time - 2) + from;
+  };
+
+  let timer = window.setInterval(function() {
+      let time = new Date().getTime() - startTime,
+      newX = easeInOutQuart(time, startX, distanceX, duration),
+      newY = easeInOutQuart(time, startY, distanceY, duration);
+      if (time >= duration) {
+          window.clearInterval(timer);
+      }
+      window.scrollTo(newX, newY);
+  }, 1000 / 60); // 60 fps
+};
 
 function makeHeadingHeights() {
+  let offset = parseInt(getComputedStyle(document.querySelector('html')).scrollPaddingTop);
   let heights = [0];
   let i = 1;
-  let offset = 168;
   while (document.querySelector( `.chapter:nth-of-type(${i})` ) != null ) {
     let heading = document.querySelector( `h3:nth-of-type(${i})` );
     heights[i] = heading.getBoundingClientRect().top + window.scrollY - offset;
@@ -31,27 +55,51 @@ function makeHeadingHeights() {
   return heights;
 }
 
+function inViewport( element ) {
+  // Get the elements position relative to the viewport
+  let infoHeight = 0 ;//document.querySelector( '.article-info-wrapper' ).offsetHeight;
+  let height = element.offsetHeight;
+  let bb = element.getBoundingClientRect();
+  // Return true when element div is scrolled above viewport
+  return (bb.top < -(height + infoHeight));
+}
+
+function inViewport2( element ) {
+  let topEdge = element.getBoundingClientRect().top;
+  let bottomEdge = element.getBoundingClientRect().bottom;
+  return ( (topEdge > 0 && bottomEdge < window.innerHeight) );
+}
+
+let headingHeights = makeHeadingHeights();
+
 // Listen for the scroll event
 document.addEventListener( 'scroll', event => {
+    
   // Show/Hide chapter list
-  let aboveElement = document.querySelector( '.article-cover' );
   let chapterWrapper = document.querySelector( '.chapter-list-wrapper' );
-  if( inViewport( aboveElement ) && window.innerWidth >= 1024 ) {
+//   let aboveElement = document.querySelector( '.article-cover' );
+//   if( inViewport( aboveElement ) && window.innerWidth >= 1024 ) {
+//     chapterWrapper.style.opacity = 1;
+//   } else {
+//     chapterWrapper.style.opacity = 0.9;
+//   }
+  let coverElement = document.querySelector( '.article-cover' );
+  if ( coverElement.getBoundingClientRect().bottom < 0 && window.innerWidth >= 1024 ) {
     chapterWrapper.style.opacity = 1;
   } else {
     chapterWrapper.style.opacity = 0;
   }
+  
 
   // Highlight current chapter
   let numChapters = document.querySelector( '.chapter-list-wrapper ul' ).childElementCount;
   let n = numChapters;
-  while (window.scrollY < headingHeights[n] && n > 0) {
+  while (window.scrollY < headingHeights[n] && n > 1) {
     n--;
   }
   for (let i = 1; i <= numChapters; i++) {
-    document.querySelector( `.chapter:nth-of-type(${i}) span` ).classList.remove('active');
+    document.querySelector( `.chapter:nth-of-type(${i}) span` ).classList.remove('chapter-current');
   }
-  document.querySelector( `.chapter:nth-of-type(${n}) span` ).classList.add('active');
-})
+  document.querySelector( `.chapter:nth-of-type(${n}) span` ).classList.add('chapter-current');
 
-let headingHeights = makeHeadingHeights();
+})
