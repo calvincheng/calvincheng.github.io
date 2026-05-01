ac3ab2c603a1c0384537865f5e24ff7b9544b1b3
Calvin Cheng <calvin.cc.cheng@gmail.com>
2026-05-01 13:23:40 +0800

    build: set 'site' in astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://www.madebycalvin.com",
   markdown: {
     shikiConfig: {
       theme: 'css-variables',
