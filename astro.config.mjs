2ac7c7cfc74b03572d3c3cd76f4e4a7521d1b346
Calvin Cheng <calvin.cc.cheng@gmail.com>
2026-07-08 12:49:49 +0800

    fix: remove unused 'css-variables' theme in shikiConfig

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ export default defineConfig({
   site: "https://www.madebycalvin.com",
   markdown: {
     shikiConfig: {
-      theme: 'css-variables',
       themes: {
         light: "github-light-high-contrast",
         dark: "github-dark-high-contrast",
